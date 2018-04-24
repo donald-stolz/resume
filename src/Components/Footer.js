a5a1dc269cada6e94588ad492be00336924a4c0c
Donald Stolz <donaldtstolz@gmail.com>
2018-04-24 10:38:14 -0500

    Added ToDos; Changed icon; Cropped Testimonials BG

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,6 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
+					 <li>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
