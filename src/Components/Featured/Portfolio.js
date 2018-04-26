e58ab825c9f8adb71c890bf9820be9e0bd5f1f01
Donald Stolz <donaldtstolz@gmail.com>
2018-04-25 22:54:27 -0500

    Fixed link issue

--- src/Components/Featured/Portfolio.js
+++ src/Components/Featured/Portfolio.js
@@ -7,18 +7,18 @@ class Portfolio extends Component {
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
-          </div>
+					<a target="_blank" href={projects.url} title={projects.title}>
+	           <div className="item-wrap">
+	             <img alt={projects.title} src={projectImage} />
+	             <div className="overlay">
+	                <div className="portfolio-item-meta">
+	               <h5>{projects.title}</h5>
+	                   <p>{projects.category}</p>
+	                </div>
+	              </div>
+	            <div className="link-icon"><i className="fa fa-link"></i></div>
+	          </div>
+					</a>
         </div>
       })
     }
