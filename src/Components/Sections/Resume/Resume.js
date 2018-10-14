c32ad39af4b63b2b3e52773b949e17abf8b8d954
Donald Stolz <donaldtstolz@gmail.com>
2018-10-13 20:06:10 -0700

    Added images & websites to volunteer; Beautified JSON

--- src/Components/Sections/Resume/Resume.js
+++ src/Components/Sections/Resume/Resume.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import Education from './Education';
 import Experience from './Experience';
 import Skills from './Skills';
+import Volunteer from './Volunteer';
 
 class Resume extends Component {
   render() {
 		console.log(this.props.data);
-		const {work, education, skills, languages} = this.props.data;
+		const { work, education, skills, languages, volunteer} = this.props.data;
 		// const volunteerEvents = volunteer.map(function(event) {
 		// 	return {
 		// 		date: event.startDate + " - " + event.endDate,
@@ -20,6 +21,7 @@ class Resume extends Component {
       <section id="resume">
 				<Experience data={work} />
 				<Education data={education}/>
+				<Volunteer data={volunteer}/>
 				<Skills skillsData={skills} languagesData={languages}/>
       </section>
     );
