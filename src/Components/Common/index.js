579f0b3cac3f503b9bd4504c0b609468fcd80bc8
Donald Stolz <donaldtstolz@gmail.com>
2018-10-20 18:48:40 -0700

    Removed MediaCard and refs

--- src/Components/Common/index.js
+++ src/Components/Common/index.js
@@ -1,5 +1,3 @@
 export { default as SocialIcons } from './SocialIcons';
 export { default as ImageIcon } from './ImageIcon';
-export { default as MediaCard } from './MediaCard';
-export { default as Timeline } from './Timeline/Timeline';
 export { default as BulletPoints } from './BulletPoints';
