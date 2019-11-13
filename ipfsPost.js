34d727ea89622dec9821f1933f36951375dfd0a6
Don Stolz <donaldtstolz@gmail.com>
2019-11-13 16:23:24 -0600

    Start script for updating IPFS

---
+++ ipfsPost.js
@@ -0,0 +1,50 @@
+//imports needed for this function
+const axios = require('axios');
+const fs = require('fs');
+const FormData = require('form-data');
+const recursive = require('recursive-fs');
+const basePathConverter = require('base-path-converter');
+
+const pinDirectoryToIPFS = (pinataApiKey, pinataSecretApiKey) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    const src = './build';
+
+    //we gather the files from a local directory in this example, but a valid readStream is all that's needed for each file in the directory.
+    recursive.readdirr(src, function(err, dirs, files) {
+        let data = new FormData();
+        files.forEach(file => {
+            //for each file stream, we need to include the correct relative file path
+            data.append(`file`, fs.createReadStream(file), {
+                filepath: basePathConverter(src, file),
+            });
+        });
+
+        // const metadata = JSON.stringify({
+        //     name: 'testname',
+        //     keyvalues: {
+        //         exampleKey: 'exampleValue',
+        //     },
+        // });
+        // data.append('pinataMetadata', metadata);
+
+        return axios
+            .post(url, data, {
+                maxContentLength: 'Infinity', //this is needed to prevent axios from erroring out with large directories
+                headers: {
+                    'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                    pinata_api_key: pinataApiKey,
+                    pinata_secret_api_key: pinataSecretApiKey,
+                },
+            })
+            .then(function(response) {
+                //handle response here
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.error(error);
+                //handle error here
+            });
+    });
+};
+
+pinDirectoryToIPFS(process.env.PINATA_KEY, process.env.PINATA_SECRET);
