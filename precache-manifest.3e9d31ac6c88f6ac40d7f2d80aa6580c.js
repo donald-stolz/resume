e5a692ad8b6a5c7a4820ced7a2d22716fc8b7d5b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-30 20:44:10 +0000

    Deploy donald-stolz/resume to github.com/donald-stolz/resume.git:gh-pages

---
+++ precache-manifest.3e9d31ac6c88f6ac40d7f2d80aa6580c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3981cb7d06927b1670dc7d799f32c280",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73207bea5b72607ebb48",
+    "url": "./static/js/2.7a8fd9a3.chunk.js"
+  },
+  {
+    "revision": "fc8bc21d8392cbf38ae828c75bee2e0a",
+    "url": "./static/js/2.7a8fd9a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c41734c53ddaa9397c4",
+    "url": "./static/js/main.dba7f693.chunk.js"
+  },
+  {
+    "revision": "d61c9661f285917f88b1",
+    "url": "./static/js/runtime-main.1a75ac8d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c163b4dcbfc8c86e3ebf6d7643f23d5.js"
+  "./precache-manifest.3e9d31ac6c88f6ac40d7f2d80aa6580c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
