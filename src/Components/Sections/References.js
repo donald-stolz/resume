4a9807baeafd5a4e0d0a602606fae4a5e8eb1de5
donald-stolz <donaldtstolz@gmail.com>
2020-11-27 15:47:50 -0500

    Intial commit for references section redesign

--- src/Components/Sections/References.js
+++ src/Components/Sections/References.js
@@ -1,52 +1,45 @@
 import React from 'react';
-import Carousel from 'nuka-carousel';
 
-const References = props => {
-    var references = props.data.map(function(reference, index) {
+const References = (props) => {
+    const references = props.data.map(function (reference, index) {
+        const divider = index + 1 === array.length ? <br /> : <hr />;
+        const company = '';
+        const website = '';
+        const workImage = 'images/work/' + company; //Not correct
+
         return (
-            <div key={reference.name}>
-                <blockquote>
-                    <p>{reference.reference}</p>
-                    <cite>{reference.name}</cite>
-                </blockquote>
+            <div className="row item" key={company}>
+                <div className="three columns image-cont">
+                    <a href={website} target="_blank" rel="noopener noreferrer">
+                        <img
+                            alt={company}
+                            className="work-pic"
+                            src={workImage}
+                        />
+                    </a>
+                </div>
+                <div className="nine columns">
+                    <div key={company}>
+                        <h3>{company}</h3>
+                        <p className="info">{reference.name}</p>
+                        <p className="details">{reference.reference}</p>
+                    </div>
+                </div>
+                {divider}
             </div>
         );
     });
 
-    const carouselConfig = {
-        autoplay: true,
-        autoplayInterval: 15000,
-        framePadding: '10px',
-        cellSpacing: 30,
-        wrapAround: true,
-    };
-
     return (
         <section id="references">
-            <div className="text-container">
-                <div className="row">
-                    <div className="two columns header-col">
-                        <h1>
-                            <span />
-                        </h1>
-                    </div>
-                    <div className="columns flex-container">
-                        <div className="flexslider">
-                            <Carousel
-                                autoplay={carouselConfig.autoplay}
-                                autoplayInterval={
-                                    carouselConfig.autoplayInterval
-                                }
-                                wrapAround={carouselConfig.wrapAround}
-                                renderCenterLeftControls={null}
-                                renderCenterRightControls={null}
-                                renderBottomCenterControls={null}
-                            >
-                                {references}
-                            </Carousel>
-                        </div>
-                    </div>
+            <div className="row reference ">
+                <div className="three columns header-col">
+                    <h1>
+                        <span>References</span>
+                    </h1>
                 </div>
+
+                <div className="twelve columns main-col">{references}</div>
             </div>
         </section>
     );
